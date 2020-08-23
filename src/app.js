5a756c4accd9d2318e74309a99c2850977503f1f
muriloignaccio <murilohsignacio@gmail.com>
2020-08-23 13:56:05 -0300

    create POST controller

--- src/app.js
+++ src/app.js
@@ -25,7 +25,21 @@ const repositories = [];
 
 app.get('/repositories', (request, response) => response.json(repositories));
 
-app.post('/repositories', (request, response) => {});
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
 
 app.put('/repositories/:id', (request, response) => {});
 
